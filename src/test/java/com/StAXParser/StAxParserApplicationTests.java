07900a5ff7c06d25c61b620fb15e7aed0e8bf38a
Developer <developer@Administrators-MacBook-Pro.local>
2021-10-29 18:22:51 +0530

    sample

---
+++ src/test/java/com/StAXParser/StAxParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StAXParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StAxParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
